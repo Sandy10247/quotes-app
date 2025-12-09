1780d15eed970087dac5477cb3e48f95ef684016
Sandy10247 <sandilyakaravadi@gmail.com>
2025-12-09 23:46:08 +0530

    clean up UI

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
-import AddQuote from './AddQuote';
 import { AuthProvider } from './AuthContext';
-// import LandingPage from './LandingPage';
-// import LandingPageTest from './LandingPageTest'
-import LandingPagePure from './LandingPagePure';
 import Dashboard from './QuoteDashboard';
-// import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -19,7 +14,6 @@ root.render(
                 <Routes>
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/add-quote" element={<AddQuote />} />
                 </Routes>
             </Router>
         </AuthProvider>
